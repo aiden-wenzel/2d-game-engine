1da451c820f1d0634607b950cd930a8f7302850d
Aiden Wenzel <wenzel.aiden@outlook.com>
2025-01-04 16:30:39 -0500

    Create game_object.c game_object.h
    - Created GameObject struct.
    - Created functions to create GameObject, render GameObject, and update
    the position of GameObjects.

---
+++ src/game_object.c
@@ -0,0 +1,36 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_render.h>
+#include <cglm/cglm.h>
+
+#include "game_object.h"
+
+
+GameObject initializeGameObject(ivec2 initialPos, ivec2 initialVel) {
+	GameObject newGameObject;
+
+	glm_ivec2_copy(initialPos, newGameObject.position);
+	glm_ivec2_copy(initialVel, newGameObject.velocity);
+
+	SDL_FRect objectShape;
+	objectShape.x = initialPos[0];
+	objectShape.y = initialPos[1];
+	objectShape.h = 5;
+	objectShape.w = 5;
+
+	newGameObject.shape = objectShape;
+
+	return newGameObject;
+}
+
+void renderGameObject(GameObject* object, SDL_Renderer* renderer) {
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 0);
+	SDL_RenderRect(renderer, &object->shape);
+}
+
+void moveGameObject(GameObject* object) {
+	object->position[0] += object->velocity[0];
+	object->position[1] += object->velocity[1];
+
+	object->shape.x = object->position[0];
+	object->shape.y = object->position[1];
+}
---
+++ src/game_object.h
@@ -0,0 +1,17 @@
+#include <SDL3/SDL_render.h>
+#include <cglm/cglm.h>
+#include <SDL3/SDL.h>
+
+typedef struct GameObject GameObject;
+
+struct GameObject {
+	ivec2 position;
+	ivec2 velocity;
+	SDL_FRect shape;
+};
+
+GameObject initializeGameObject(ivec2 initialPos, ivec2 initialVel);
+
+void renderGameObject(GameObject* object, SDL_Renderer* renderer);
+
+void moveGameObject(GameObject* object);
